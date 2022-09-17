587ca1a62e5644c39c57ff489c59a578bff03181
yexjin <tnska0421@naver.com>
2022-09-17 10:37:45 +0900

    BOJ 9/17

---
+++ algorithm-study/BOJ/1000.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+var val = readLine()!.split(separator: " ")
+
+print(Int(val[0])!+Int(val[1])!)
---
+++ algorithm-study/BOJ/1008.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+var val = readLine()!.split(separator: " ")
+
+var result = Double(val[0])! / Double(val[1])!
+
+print(result)
