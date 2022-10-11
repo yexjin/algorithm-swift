1d7f0645d40c7929715e00362ba2062426f9ace1
yexjin <tnska0421@naver.com>
2022-10-12 01:30:52 +0900

    4999

---
+++ algorithm-study/BOJ/Bronze/4999.swift
@@ -0,0 +1,17 @@
+//
+//  4999.swift
+//  algorithm-study
+//
+//  Created by 오예진 on 2022/10/12.
+//
+
+import Foundation
+
+let doc = readLine()!
+let pat = readLine()!
+
+if doc.count >= pat.count {
+    print("go")
+} else {
+    print("no")
+}
